64f315b3bdd84e281d5039e0a110320ae960ad10
Patricio <patriciobordon123@gmail.com>
2021-12-12 15:59:08 -0300

    Add files via upload

---
+++ Practica UTN/Hola_Mundo/hola_mundo.cpp
@@ -0,0 +1,9 @@
+//Hola este es mi primer hola mundo
+
+#include <iostream>
+
+int main(){
+    std::cout << "Hola Mundo";
+    std::cin.get();
+    return 0;
+}
---
+++ Practica UTN/TiposDatos/tiposDatos.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main(){
+    int entero=10;
+    float flotante=9.99;
+    double doble=9.9575985;
+    char caracter='A';
+    std::cout<<caracter;
+    std::cin.get();
+    return 0;
+}
