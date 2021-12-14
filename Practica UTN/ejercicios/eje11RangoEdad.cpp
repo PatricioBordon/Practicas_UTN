146570bb1e4456afc5f8a8eed0ad32627240c7cc
PatricioBordon <patriciobordon123@gmail.com>
2021-12-14 20:47:13 -0300

    Updated

---
+++ Practica UTN/ejercicios/eje11RangoEdad.cpp
@@ -0,0 +1,16 @@
+/*Una empresa esta contratando personas entre
+18 y 40 años. Hacer un programa que determine 
+si la persona es apta dependiendo la edad.*/
+#include <iostream>
+int main(){
+    int edad;
+    std::cout<<"Ingresar edad del candidato/a: ";std::cin>>edad;
+    if(edad>=18 && edad<=40){
+        std::cout<<"Es apto/a para el empleo\n";
+    }
+    else{
+        std::cout<<"No es apto/a para el empleo\n";
+    }
+    system("pause");
+    return 0;
+}
