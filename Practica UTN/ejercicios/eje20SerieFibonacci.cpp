363b2d017e97e15cfa536b43f0d05cf38b6e59ca
PatricioBordon <patriciobordon123@gmail.com>
2021-12-18 14:30:48 -0300

    Updated

---
+++ Practica UTN/ejercicios/eje20SerieFibonacci.cpp
@@ -0,0 +1,26 @@
+/*
+Hacer un programa que pida un numero entero n
+Validar que el numero n sea mayor a 1
+Bucle que genere una serie fibonacci de 1 hasta n
+1 1 2 3 5 8 13 21 34....n
+*/
+#include <iostream>
+int main(){
+    int n, x=0,y=1,z=0;
+    do
+    {
+        std::cout<<"Digitar un numero mayor a 1: ";std::cin>>n;
+    } while (n<1);
+    std::cout<<"1 ";
+    for (int i = 1; i <= n; i++)
+    {
+        z=x+y;
+        std::cout<<z<<" ";
+        x=y;
+        y=z;
+    }
+    std::cout<<"\n";
+
+    system("pause");
+    return 0;
+}
